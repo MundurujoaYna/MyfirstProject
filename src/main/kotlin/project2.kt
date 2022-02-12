a6cf74c3f77f1ab73121af0f904da1143cebfd83
Mundurujoana <mundujoana@gmail.com>
2022-02-12 09:11:26 +0300

    first commit

---
+++ src/main/kotlin/project2.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var letter: Char = 'g'
+    println(letter)
+    var anitaB = arrayOf("joana", "shamim", "susan","mercy")
+    println(anitaB[3])
+    var numbers = arrayOf(1, 2, 3, 5, 6,7)
+    println(numbers[4])
+    var stingNum = arrayOf("max", 1, 'a', 0.53, "true")
+    println(stingNum[3])
+}
